db3761bf9a26065a2098d47d72c5f059cbb76c78
diegoalfonsoc18 <diegoalfonsoc18@example.com>
2024-12-18 16:35:23 -0500

    initial configuration

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Divice</Text>
+      <Text>Hola Diego</Text>
       <StatusBar style="auto" />
     </View>
   );
