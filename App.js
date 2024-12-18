e26275ad78fe9059eb528e2d48e31ffcd40a8779
diegoalfonsoc18 <dlalfonso57@gmail.com>
2024-12-18 17:10:20 -0500

    Hello world

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Diego</Text>
+      <Text>Hola Mundo</Text>
       <StatusBar style="auto" />
     </View>
   );
