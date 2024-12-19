36c8aa79a5ca417de6993c0f6378ea5652b29714
diegoalfonsoc18 <dlalfonso57@gmail.com>
2024-12-19 15:19:52 -0500

    fixed yarn configuration

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet } from "react-native";
 import NavigationStack from "./src/components/navigation/NavigationStack";
 import NavigationTab from "./src/components/navigation/NavigationTab";
+import NavigationDrawer from "./src/components/navigation/NavigationDrawer";
 export default function App() {
   return (
     <NavigationContainer>
       {/* <NavigationStack /> */}
-      <NavigationTab />
+      {/* <NavigationTab /> */}
+      <NavigationDrawer />
     </NavigationContainer>
   );
 }
